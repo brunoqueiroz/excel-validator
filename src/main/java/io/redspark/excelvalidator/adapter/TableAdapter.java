61ad9d91411c4005c89f248d80717ab41c392744
Bruno Queiroz <bruno.queiroz@redspark.io>
2018-09-17 12:23:12 -0300

    formatacao

--- src/main/java/io/redspark/excelvalidator/adapter/TableAdapter.java
+++ src/main/java/io/redspark/excelvalidator/adapter/TableAdapter.java
@@ -1,28 +1,32 @@
 package io.redspark.excelvalidator.adapter;
 
 import io.redspark.excelvalidator.builders.CellBuilder;
-import io.redspark.excelvalidator.model.Cell;
 import io.redspark.excelvalidator.model.Line;
 import io.redspark.excelvalidator.model.Table;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
 public interface TableAdapter {
 
     List<Line> getLines(Table table, List<CellBuilder> cells);
+
     CellStyle getCellStyle();
+
     String createSheet(String path) throws IOException;
+
     Line getHeader();
 
     String createSheet(String path, String name) throws IOException;
 
     Boolean hasHeader();
+
     String getHeaderNameOf(int index);
+
     String getCellValue(Row currentRow, int index);
+
     void cellTint(Line line, CellStyle styleYellow, int index);
 
 }
